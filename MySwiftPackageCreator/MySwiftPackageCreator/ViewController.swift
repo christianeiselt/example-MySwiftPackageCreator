fd6982d6bc18e86c5995241efc817a205363c2fa
Christian Eiselt <dev@eiselt.ch>
2021-10-23 16:52:12 +0200

    Initial setup

---
+++ MySwiftPackageCreator/MySwiftPackageCreator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySwiftPackageCreator
+//
+//  Created by Christian Eiselt on 23.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
